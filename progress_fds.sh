23d20ba378a99426f01ce3887d02dd7116ff03a8
Azat Khuzhin <a3at.mail@gmail.com>
2013-05-24 13:39:10 +0000

    progress_fds.sh
    From https://gist.github.com/azat/2830255
    
    Squashed commit of the following:
    
    commit c8a48be481be8e86d460e68369a50f8db532a3ba
    Author: Azat Khuzhin <a3at.mail@gmail.com>
    Date:   Thu Apr 18 16:15:04 2013 +0000
    
        Add Usage dialog
    
    commit ae5612fad947c603f6b4d3b8b71e8506fe63785e
    Author: Azat Khuzhin <a3at.mail@gmail.com>
    Date:   Thu Apr 18 16:12:48 2013 +0000
    
        Drop duplicate code segments
    
    commit 33a7a2768063c5c883c2223d9610034c757e797a
    Author: Azat Khuzhin <a3at.mail@gmail.com>
    Date:   Thu Apr 18 15:30:09 2013 +0000
    
        Make progress_fds executable
    
    commit d633d06cb223592e3c17697112278520b2973d6a
    Author: Azat Khuzhin <a3at.mail@gmail.com>
    Date:   Thu Apr 18 15:27:25 2013 +0000
    
        Ability to measure block devices added
    
        Useful to know progress of resize2fs for example.
    
    commit b7d5d392af980ce3fba12f4a270cae1d4f2d7b06
    Author: Azat Khuzhin <a3at.mail@gmail.com>
    Date:   Thu Apr 18 15:18:52 2013 +0000
    
        Drop first check for file exist
    
    commit 844f5a614c048768b84ccdf7eab70948db101626
    Author: Azat Khuzhin <a3at.mail@gmail.com>
    Date:   Mon Mar 18 15:41:30 2013 +0000
    
    commit 1c329788e04d6651d75d9c1ec05289cc8a9d10ed
    Author: Azat Khuzhin <a3at.mail@gmail.com>
    Date:   Sat Jun 16 09:43:16 2012 -0700
    
    commit 9aac0b412c5d3eb6ea567f56bb0b38171d8a186c
    Author: Azat Khuzhin <a3at.mail@gmail.com>
    Date:   Sat Jun 16 09:32:45 2012 -0700
    
    commit 826722e44335f995c812a540cd9d4706f47ca604
    Author: Azat Khuzhin <a3at.mail@gmail.com>
    Date:   Tue May 29 12:42:12 2012 -0700

---
+++ progress_fds.sh
@@ -0,0 +1,71 @@
+#!/usr/bin/env bash
+
+#
+# Progress by file descriptors (cp / gz / resize2fs e.t.c.)
+#
+# 2013-04-18:
+# Ability to measure block devices added
+# Useful to know progress of resize2fs for example.
+# 
+# Run using `watch` util
+#
+# Original available here:
+# https://gist.github.com/azat/2830255
+#
+
+if [ $# -lt 1 ]; then
+        echo "Usage: $0 /path/to/file [ PID]" >&2
+        exit 2
+fi
+
+SRCFILE=$1
+PID=$2
+
+# Not existed file, or not set
+if [ "x$SRCFILE" = "x" ]; then
+        echo "File $SRCFILE not exist"
+        exit 1
+fi
+SRCFILE=$(readlink -f $SRCFILE)
+
+# Maybe not regular file
+DEV=0
+if [ ! -f $SRCFILE ]; then
+        if $(file $SRCFILE 2>/dev/null | grep -q "sticky block special"); then
+                DEV=1
+        else
+                echo "File $SRCFILE is not exist"
+                exit 1
+        fi
+fi
+
+# Find lsof, or PID
+LSOF=`which lsof`
+if [ "x$LSOF" = "x" ] && [ "x$PID" = "x" ]; then
+        echo "No 'lsof' or PID"
+        exit 1
+fi
+
+# Find fdinfo file
+if [ "x$PID" = "x" ]; then
+        PROC_FD=$( lsof $SRCFILE | head -n2 | tail -n1 | awk '{printf "/proc/%u/fdinfo/%u", $2, substr($4, 1, length($4)-1)}' )
+else
+        PROC_FD_BASENAME=$( ls -l /proc/$PID/fd 2>&1 | grep "$SRCFILE" | head -n1 | sed -r 's/.+ ([0-9]+)\s+->.+/\1/' )
+        PROC_FD="/proc/$PID/fdinfo/$PROC_FD_BASENAME"
+fi
+
+# Get fd info
+PROC_FD_INFO=$( head -n1 "$PROC_FD" 2>/dev/null )
+if [ $? -ne 0 ]; then
+        echo "Can't access to $PROC_FD"
+else
+        # Echo processes
+        FILESIZE=0
+        if [ $DEV -eq 0 ]; then
+                FILESIZE=$(wc -c $SRCFILE | awk '{print $1}')
+        else
+                FILESIZE=$(blockdev --getsize64 $SRCFILE)
+        fi
+
+        echo $(( `echo $PROC_FD_INFO | awk '{print $2}'` * 100 / $FILESIZE )) %
+fi
