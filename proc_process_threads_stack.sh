634b84d73d0fd9b28942057078cce7fac6c47893
Azat Khuzhin <a3at.mail@gmail.com>
2014-10-11 20:00:18 +0400

    proc_process_threads_stack: use /proc/self/task/*/stack

--- proc_process_threads_stack.sh
+++ proc_process_threads_stack.sh
@@ -2,6 +2,5 @@
 
 # see https://github.com/azat/linux/commit/5b0b442cdc1049
 
-ls /proc/$1/maps | \
-    xargs grep stack: | cut -d: -f3 | tr -d ] | sort -n | \
-    xargs -I{} sh -c "echo {} && cat /proc/{}/stack && echo"
+ls /proc/$1/task/*/stack | \
+    xargs -I{} sh -c "echo {} && cat {} && echo"
