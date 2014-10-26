df75888f4c9c5c3c3beb75d738a237e595139733
Azat Khuzhin <a3at.mail@gmail.com>
2014-10-26 17:36:54 +0300

    proc_smaps_referenced: add total line, and make awk script more beauty

--- proc_smaps_referenced.sh
+++ proc_smaps_referenced.sh
@@ -1,7 +1,26 @@
 #!/usr/bin/env bash
 
 # examples:
-# proc_smaps_referenced.sh 10015 | tr -d , | awk '($2 > 8192) && ($(NF-1) > 0) && ($(NF-1) < 80)'
+# proc_smaps_referenced.sh $$ | tr -d , | awk '($2 > 8192) && ($(NF-1) > 0) && ($(NF-1) < 80)'
+
+script=$(cat - <<'EOF'
+/^(Size|Referenced):/ {
+    if ($1 ~ /^Size:/) {
+        size = $(NF-1);
+        tsize += size;
+    } else {
+        refs = $(NF-1);
+        trefs += refs;
+        p = size ? (refs / size * 100) : 0;
+        printf("size: %10.f, referenced: %10.f (%7.2f %%)\n", size, refs, p);
+    }
+}
+END {
+    p = tsize ? (trefs / tsize * 100) : 0;
+    printf("[total] size: %10.f, referenced: %10.f (%7.2f %%)\n", tsize, trefs, p);
+}
+EOF
+)
 
 smaps=/proc/$1/smaps
-awk '/^(Size|Referenced):/ { if ($1 ~ /^Size:/) { size = $(NF-1) } else { refs = $(NF-1); printf("size: %10.f, referenced: %10.f (%7.2f %%)\n", size, refs, size ? (refs / size * 100) : 0) } }' < $smaps
+awk "$script" < $smaps
