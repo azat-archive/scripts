3b7865a590dde6288c5b214ddd98a238fc2b58e1
Azat Khuzhin <a3at.mail@gmail.com>
2014-11-28 16:51:36 +0300

    mon_free_swap: monitor free swap and execute command if it too low

---
+++ mon_free_swap.sh
@@ -0,0 +1,48 @@
+#!/usr/bin/env bash
+
+# Useful to kill/stop process if it will eat to much memory
+
+maxPercents=97
+interval=10
+cmd=:
+
+function printUsage()
+{
+    echo "$0 [ -m max_percents ] [ -i interval_secs ] [ cmd ]" >&2
+    exit 1
+}
+
+function parseOptions()
+{
+    local OPTIND o
+    while getopts "m:i:" o; do
+        case "$o" in
+            m) maxPercents=$OPTARG;;
+            i) interval=$OPTARG;;
+            *) printUsage;;
+        esac
+    done
+    shift $((OPTIND-1))
+    [ -n "$*" ] && cmd="$@"
+}
+
+function swapUsedPercents()
+{
+    free -m | awk '/Swap:/ {printf("%.f\n", ($3/$2)*100);}'
+}
+
+function main()
+{
+    while :; do
+        sleep $interval
+        if [ $(swapUsedPercents) -lt $maxPercents ]; then
+            continue
+        fi
+        echo "Too much swap used, executing user specified command"
+        $cmd
+        break
+    done
+}
+
+parseOptions "$@"
+main
