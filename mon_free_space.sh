1feee0f11511062bb1d145b279a0514e982e8434
Azat Khuzhin <a3at.mail@gmail.com>
2014-12-10 03:49:21 +0300

    mon_free_space: fix diskUsedPercents() to work not only with mount points
    df(1) could to this!

--- mon_free_space.sh
+++ mon_free_space.sh
@@ -31,7 +31,7 @@ function parseOptions()
 function diskUsedPercents()
 {
     local disk=$1
-    df -m | awk -vdisk=$disk '$0 ~ disk "$" { print substr($5, 1, length($5) - 1) }'
+    df -m $disk | awk '{ print substr($5, 1, length($5) - 1) }'
 }
 
 function main()
